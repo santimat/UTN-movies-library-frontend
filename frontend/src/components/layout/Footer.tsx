eef61f6272b794416cb58267fcc44abb92220f25
santimat <santimaturo1@gmail.com>
2026-05-29 12:48:41 -0300

    feat: create a basic footer

---
+++ frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+  return (
+    <footer className="bg-neutral px-4 py-8 text-white uppercase">
+      <h4 className="font-body text-2xl font-bold">Biblioteca de peliculas</h4>
+      <p className="text-xs text-white/60">
+        &copy; Biblioteca de peliculas. Deja tu reseña y disfruta de compartir
+        tu opinión con los demás.
+      </p>
+    </footer>
+  );
+}
