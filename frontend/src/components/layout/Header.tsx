a19e0f4cb73d27c310bfb07cbcf548c89438f4e1
santimat <santimaturo1@gmail.com>
2026-05-29 12:16:46 -0300

    feat: apply grid and border for header

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,11 +1,9 @@
-import { NavBar } from "@/components/layout/NavBar";
+import { NavBar } from '@/components/layout/NavBar';
 
 export function Header() {
   return (
-    <header>
-      <h1 className="font-headline text-3xl font-bold text-neutral">
-        Biblioteca de peliculas
-      </h1>
+    <header className="grid grid-cols-3 justify-center border-b-2 border-neutral p-4 font-headline text-neutral uppercase outline-2">
+      <h1 className="text-3xl font-bold">Biblioteca de peliculas</h1>
       <NavBar />
     </header>
   );
