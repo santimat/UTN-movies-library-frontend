f185692dc62b9f1a5edb7ba39aefd891169d18c1
santimat <santimaturo1@gmail.com>
2026-05-29 12:45:31 -0300

    refactor: wrap all routes inside of <main> element

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
-import { Header } from '@/components/layout/Header';
 import { Routes, Route } from 'react-router';
 import { Home } from '@/pages/Home';
+import { Header } from '@/components/layout/Header';
+import { Footer } from '@/components/layout/Footer';
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <main className="my-10 flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+      <Footer />
     </>
   );
 }
