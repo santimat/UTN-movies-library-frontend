5ffb6a7e3f5ba74d3f3afd6985de0573b0f4ee7b
santimat <santimaturo1@gmail.com>
2026-05-29 12:45:08 -0300

    refactor: remove main container

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,11 +2,9 @@ export function Home() {
   return (
     <>
       <title>Biblioteca de Películas</title>
-      <main className="my-10">
-        <h2 className="font-headline text-2xl font-bold text-neutral">
-          Probando 1 2 3
-        </h2>
-      </main>
+      <h2 className="font-headline text-2xl font-bold text-neutral">
+        Probando 1 2 3
+      </h2>
     </>
   );
 }
