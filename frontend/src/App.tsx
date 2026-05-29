6ad38d2f3c4a1367650d526e3376962c86e987a1
santimat <santimaturo1@gmail.com>
2026-05-29 11:39:54 -0300

    feat: add <Header/> component outside routes and add <Routes> to handle routing, with index route /

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
+import { Header } from "@/components/layout/Header";
+import { Routes, Route } from "react-router";
+import { Home } from "@/pages/Home";
+
 function App() {
-  return <h1>Starting project</h1>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
