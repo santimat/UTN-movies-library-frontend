3a65d91db80462059880fb04f8b3fdaa86340486
santimat <santimaturo1@gmail.com>
2026-05-29 12:17:24 -0300

    feat: add style for active routes and list display

--- frontend/src/components/layout/NavBar.tsx
+++ frontend/src/components/layout/NavBar.tsx
@@ -1,26 +1,33 @@
-import { NavLink } from "react-router";
+import { NavLink } from 'react-router';
 
 const navItems = [
   {
-    name: "Peliculas",
-    href: "/",
+    name: 'Peliculas',
+    href: '/',
   },
   {
-    name: "Series",
-    href: "/series",
+    name: 'Series',
+    href: '/series',
   },
   {
-    name: "Mi Lista",
-    href: "/my-list",
+    name: 'Mi Lista',
+    href: '/my-list',
   },
 ] as const;
 
 export function NavBar() {
+  const handleIsActive = ({ isActive }: { isActive: boolean }) => {
+    return isActive
+      ? 'border-b-3 border-secondary'
+      : 'text-neutral/50 hover:bg-neutral/50 hover:text-white py-1 px-2 transition-colors';
+  };
   return (
-    <ul>
+    <ul className="flex justify-center gap-4 font-label text-xl font-semibold">
       {navItems.map(({ name, href }) => (
-        <li>
-          <NavLink to={href}>{name}</NavLink>
+        <li key={href}>
+          <NavLink className={handleIsActive} to={href}>
+            {name}
+          </NavLink>
         </li>
       ))}
     </ul>
