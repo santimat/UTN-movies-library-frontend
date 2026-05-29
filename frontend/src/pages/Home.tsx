8ac03487340a46e5571b0f274b9cfedff4a98936
santimat <santimaturo1@gmail.com>
2026-05-29 12:18:41 -0300

    feat: add title tag and margin-block to main

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,12 @@
 export function Home() {
   return (
-    <main>
-      <h2 className="font-headline text-2xl font-bold text-neutral">
-        Probando 1 2 3
-      </h2>
-    </main>
+    <>
+      <title>Biblioteca de Películas</title>
+      <main className="my-10">
+        <h2 className="font-headline text-2xl font-bold text-neutral">
+          Probando 1 2 3
+        </h2>
+      </main>
+    </>
   );
 }
