67a8f002f6f53fa7d063603ccc5c4222490ce0f4
santimat <santimaturo1@gmail.com>
2026-05-29 15:44:12 -0300

    feat: create a little test to try rating select functionality

---
+++ src/pages/Test.tsx
@@ -0,0 +1,53 @@
+import { type MouseEvent, useEffect, useState } from 'react';
+import { Star } from '@/components/icons/Star';
+
+export function Test() {
+  const [rating, setRating] = useState(() => {
+    const savedRating = localStorage.getItem('rating');
+    return savedRating ? +savedRating : 0;
+  });
+  // init an array of 5 elements with values from 1 to 5
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+
+  const handleClick = (event: MouseEvent<HTMLUListElement>) => {
+    const { target } = event;
+    if (target instanceof HTMLLIElement) {
+      // + converts the string to a number, if the dataset.rating is undefined it will default to '0'
+      const clickedRating = +(target.dataset.rating || '0');
+      if (!clickedRating) return;
+      // is current rating is the same as clickedRating, the reset to 0
+      if (rating === clickedRating) return setRating(0);
+
+      setRating(clickedRating);
+    }
+  };
+
+  useEffect(() => {
+    // save in localStorage
+    localStorage.setItem('rating', rating.toString());
+  }, [rating]);
+
+  return (
+    <>
+      {rating ? (
+        <p className="text-center text-2xl font-bold">
+          Esta plubicación tiene un rating de {rating} estrellas
+        </p>
+      ) : null}
+      <ul className="flex justify-center" onClick={handleClick}>
+        {stars.map((star) => (
+          <li
+            className="transition-transform hover:scale-105 hover:cursor-pointer active:scale-95"
+            key={`star-${star}`}
+            data-rating={star}
+            role="button"
+          >
+            <Star
+              className={`pointer-events-none w-20 ${star <= rating ? 'fill-amber-400' : null}`}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
