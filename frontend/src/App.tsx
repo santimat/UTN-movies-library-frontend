2c38f7d7fab0a20b58e8a811713e59fb991b1af8
santimat <santimaturo1@gmail.com>
2026-05-29 12:16:02 -0300

    refactor: migrate to single quote

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import { Header } from "@/components/layout/Header";
-import { Routes, Route } from "react-router";
-import { Home } from "@/pages/Home";
+import { Header } from '@/components/layout/Header';
+import { Routes, Route } from 'react-router';
+import { Home } from '@/pages/Home';
 
 function App() {
   return (
