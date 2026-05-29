da093861efef58ba175c38da407fc0c051dd5fbd
santimat <santimaturo1@gmail.com>
2026-05-29 12:35:25 -0300

    feat: render authButton

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,10 +1,17 @@
 import { NavBar } from '@/components/layout/NavBar';
+import { AuthButton } from '@/components/ui/AuthButton';
 
 export function Header() {
   return (
-    <header className="grid grid-cols-3 justify-center border-b-2 border-neutral p-4 font-headline text-neutral uppercase outline-2">
-      <h1 className="text-3xl font-bold">Biblioteca de peliculas</h1>
+    <header className="grid grid-cols-3 items-center justify-center border-b-2 border-neutral p-4 text-neutral uppercase outline-2">
+      <h1 className="font-headline text-3xl font-bold">
+        Biblioteca de peliculas
+      </h1>
       <NavBar />
+      <div className="flex justify-end gap-4">
+        <AuthButton text="Inciar sesión" href="/login" />
+        <AuthButton text="Registrarse" href="/sign-in" />
+      </div>
     </header>
   );
 }
