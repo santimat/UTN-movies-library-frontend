df3ef6c495d02063c5710e4485db7c5bb1b2a9d0
santimat <santimaturo1@gmail.com>
2026-05-29 15:50:56 -0300

    feat: add home link for <h1>

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,12 @@
+import { NavLink } from 'react-router';
 import { NavBar } from '@/components/layout/NavBar';
 import { AuthButton } from '@/components/ui/AuthButton';
 
 export function Header() {
   return (
     <header className="grid grid-cols-3 items-center justify-center border-b-2 border-neutral p-4 text-neutral uppercase outline-2">
-      <h1 className="font-headline text-3xl font-bold">
-        Biblioteca de peliculas
+      <h1 className="font-headline text-2xl font-bold">
+        <NavLink to={'/'}>Biblioteca de peliculas</NavLink>
       </h1>
       <NavBar />
       <div className="flex justify-end gap-4">
