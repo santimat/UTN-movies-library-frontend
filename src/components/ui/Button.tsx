fca5925f8a5ca03e34a761ef58b75a6d91347d4a
santimat <santimaturo1@gmail.com>
2026-05-30 12:11:29 -0300

    refactor: remove w-fit for <Button/>

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 export function Button({ text, href, className: classes }: ButtonProps) {
   return (
     <NavLink
-      className={`w-fit border-2 border-neutral p-2 font-bold whitespace-nowrap transition-transform hover:-translate-x-0.5 hover:-translate-y-0.5 active:scale-95 ${classes}`}
+      className={`border-2 border-neutral p-2 font-bold whitespace-nowrap transition-transform hover:-translate-x-0.5 hover:-translate-y-0.5 active:scale-95 ${classes}`}
       to={href}
     >
       {text}
