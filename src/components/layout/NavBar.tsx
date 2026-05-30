361169c554eb4df1213c5d108454612e9aca4fbc
santimat <santimaturo1@gmail.com>
2026-05-30 12:12:46 -0300

    feat: split Desktop and Movile nav into two different components

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,39 +1,56 @@
-import { href, NavLink } from 'react-router';
-
-const navItems = [
+import { useState } from 'react';
+import type { HeaderNavItem } from '@/types/HeaderNavItem';
+import { useMediaQuery } from '@/hooks/useMediaQuery';
+import { BurguerIcon } from '@/components/icons/Burguer';
+import { DesktopNav } from '@/components/layout/DesktopNav';
+import { MobileNav } from '@/components/layout/MobileNav';
+const navItems: HeaderNavItem[] = [
   {
-    name: 'Todo',
+    text: 'Todo',
     href: '/',
   },
   {
-    name: 'Peliculas',
+    text: 'Peliculas',
     href: '/movies',
   },
   {
-    name: 'Series',
+    text: 'Series',
     href: '/series',
   },
   {
-    name: 'Mi Lista',
+    text: 'Mi Lista',
     href: '/my-list',
   },
 ] as const;
 
 export function NavBar() {
+  // For mobile navbar
+  const [isOpen, setIsOpen] = useState(false);
+  const isDesktop = useMediaQuery('(min-width:768px)');
+
+  const handleClick = () => setIsOpen((prev) => !prev);
   const handleIsActive = ({ isActive }: { isActive: boolean }) => {
     return isActive
       ? 'border-b-3 border-secondary'
       : 'text-neutral/50 hover:bg-neutral/50 hover:text-white py-1 px-2 transition-colors';
   };
+
+  if (isDesktop)
+    return <DesktopNav navItems={navItems} handleIsActive={handleIsActive} />;
+
   return (
-    <ul className="flex justify-center gap-4 font-label text-xl font-semibold">
-      {navItems.map(({ name, href }) => (
-        <li key={href}>
-          <NavLink className={handleIsActive} to={href}>
-            {name}
-          </NavLink>
-        </li>
-      ))}
-    </ul>
+    <>
+      <button
+        onClick={handleClick}
+        className="relative z-20 hover:cursor-pointer active:scale-95"
+      >
+        <BurguerIcon className="pointer-events-none} w-8" />
+      </button>
+      <MobileNav
+        navItems={navItems}
+        handleIsActive={handleIsActive}
+        isOpen={isOpen}
+      />
+    </>
   );
 }
