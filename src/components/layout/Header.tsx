10c33802c6049ab606ca1ce722a194779a2e31dd
santimat <santimaturo1@gmail.com>
2026-05-30 12:11:52 -0300

    style: switch from grid to tailwind for Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,18 +1,13 @@
 import { NavLink } from 'react-router';
 import { NavBar } from '@/components/layout/NavBar';
-import { AuthButton } from '@/components/ui/AuthButton';
 
 export function Header() {
   return (
-    <header className="grid grid-cols-3 items-center justify-center border-b-2 border-neutral p-4 text-neutral uppercase outline-2">
+    <header className="flex items-center justify-between border-b-4 border-neutral p-4">
       <h1 className="font-headline text-2xl font-bold">
         <NavLink to={'/'}>Biblioteca de peliculas</NavLink>
       </h1>
       <NavBar />
-      <div className="flex justify-end gap-4">
-        <AuthButton text="Inciar sesión" href="/login" />
-        <AuthButton text="Registrarse" href="/sign-in" />
-      </div>
     </header>
   );
 }
