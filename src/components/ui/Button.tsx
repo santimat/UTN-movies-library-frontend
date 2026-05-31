e587b1f1820f1ad9dea483387c02e2480335b650
santimat <santimaturo1@gmail.com>
2026-05-31 19:05:52 -0300

    feat: hide auth button when /login is the current path

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -8,11 +8,11 @@ interface ButtonProps {
 }
 
 export function Button({ href, className: classes, children }: ButtonProps) {
+  const handleIsActive = ({ isActive }: { isActive: boolean }) => {
+    return `border-2 p-2 font-bold whitespace-nowrap transition-transform hover:-translate-x-0.5 hover:-translate-y-0.5 border-neutral active:scale-95 ${isActive && 'hidden'} ${classes}`;
+  };
   return (
-    <NavLink
-      className={`border-2 border-neutral p-2 font-bold whitespace-nowrap transition-transform hover:-translate-x-0.5 hover:-translate-y-0.5 active:scale-95 ${classes}`}
-      to={href}
-    >
+    <NavLink className={handleIsActive} to={href}>
       {children}
     </NavLink>
   );
