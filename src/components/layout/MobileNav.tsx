0c5fef3760a902b9ef8fb70da10501d81b9fa1ea
santimat <santimaturo1@gmail.com>
2026-05-31 18:37:35 -0300

    feat: move all logic for MobileNav component to the same component

--- src/components/layout/MobileNav.tsx
+++ src/components/layout/MobileNav.tsx
@@ -1,31 +1,47 @@
-import type { HeaderNavItem } from '@/types/HeaderNavItem';
+import { useState, type MouseEvent } from 'react';
 import { NavLink } from 'react-router';
+import type { HeaderNavItem } from '@/types/HeaderNavItem';
+import { BurguerIcon } from '@/components/icons/Burguer';
 
 interface MobileNavProps {
   navItems: HeaderNavItem[];
   handleIsActive: ({ isActive }: { isActive: boolean }) => string;
-  isOpen: boolean;
 }
 
-export function MobileNav({
-  navItems,
-  handleIsActive,
-  isOpen,
-}: MobileNavProps) {
+export function MobileNav({ navItems, handleIsActive }: MobileNavProps) {
+  const [isOpen, setIsOpen] = useState(true);
   const showNav = isOpen ? '' : 'translate-x-full';
+  const handleClick = () => setIsOpen((prev) => !prev);
+
+  const handleNavClick = (event: MouseEvent<HTMLUListElement>) => {
+    const target = event.target as HTMLLIElement;
+    const isLink = target.tagName === 'A';
+    if (isLink) {
+      setIsOpen(false);
+    }
+  };
+
   return (
-    <nav
-      className={`absolute right-0 bottom-0 z-10 h-dvh w-3/4 bg-white/80 backdrop-blur-lg transition-transform duration-500 ${showNav}`}
-    >
-      <ul>
-        {navItems.map(({ text, href }) => (
-          <li key={href}>
-            <NavLink className={handleIsActive} to={href}>
-              {text}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <>
+      <button
+        onClick={handleClick}
+        className="relative z-20 hover:cursor-pointer active:scale-95"
+      >
+        <BurguerIcon className="pointer-events-none} w-8" />
+      </button>
+      <nav
+        className={`absolute right-0 bottom-0 z-10 h-dvh w-3/4 bg-white/80 backdrop-blur-lg transition-transform duration-500 ${showNav} flex flex-col items-center justify-center`}
+      >
+        <ul className="grid gap-5 text-center" onClick={handleNavClick}>
+          {navItems.map(({ text, href }) => (
+            <li key={href}>
+              <NavLink className={handleIsActive} to={href}>
+                {text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
   );
 }
--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,9 +1,8 @@
-import { useState } from 'react';
 import type { HeaderNavItem } from '@/types/HeaderNavItem';
 import { useMediaQuery } from '@/hooks/useMediaQuery';
-import { BurguerIcon } from '@/components/icons/Burguer';
 import { DesktopNav } from '@/components/layout/DesktopNav';
 import { MobileNav } from '@/components/layout/MobileNav';
+
 const navItems: HeaderNavItem[] = [
   {
     text: 'Todo',
@@ -25,32 +24,18 @@ const navItems: HeaderNavItem[] = [
 
 export function NavBar() {
   // For mobile navbar
-  const [isOpen, setIsOpen] = useState(false);
   const isDesktop = useMediaQuery('(min-width:768px)');
 
-  const handleClick = () => setIsOpen((prev) => !prev);
   const handleIsActive = ({ isActive }: { isActive: boolean }) => {
-    return isActive
-      ? 'border-b-3 border-secondary'
-      : 'text-neutral/50 hover:bg-neutral/50 hover:text-white py-1 px-2 transition-colors';
+    return `p-2 ${
+      isActive
+        ? 'border-b-3 border-secondary'
+        : 'text-neutral/50 hover:bg-neutral/50 hover:text-white transition-colors'
+    }`;
   };
 
   if (isDesktop)
     return <DesktopNav navItems={navItems} handleIsActive={handleIsActive} />;
 
-  return (
-    <>
-      <button
-        onClick={handleClick}
-        className="relative z-20 hover:cursor-pointer active:scale-95"
-      >
-        <BurguerIcon className="pointer-events-none} w-8" />
-      </button>
-      <MobileNav
-        navItems={navItems}
-        handleIsActive={handleIsActive}
-        isOpen={isOpen}
-      />
-    </>
-  );
+  return <MobileNav navItems={navItems} handleIsActive={handleIsActive} />;
 }
