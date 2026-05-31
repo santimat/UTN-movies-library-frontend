02eb8e7827e871ba66049131a32c19c2d411e473
santimat <santimaturo1@gmail.com>
2026-05-31 18:44:21 -0300

    feat: button takes children

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,18 +1,19 @@
+import type { ReactNode } from 'react';
 import { NavLink } from 'react-router';
 
 interface ButtonProps {
-  text: string;
+  children: ReactNode;
   href: string;
   className?: string;
 }
 
-export function Button({ text, href, className: classes }: ButtonProps) {
+export function Button({ href, className: classes, children }: ButtonProps) {
   return (
     <NavLink
       className={`border-2 border-neutral p-2 font-bold whitespace-nowrap transition-transform hover:-translate-x-0.5 hover:-translate-y-0.5 active:scale-95 ${classes}`}
       to={href}
     >
-      {text}
+      {children}
     </NavLink>
   );
 }
