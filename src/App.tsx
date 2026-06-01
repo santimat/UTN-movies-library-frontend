231f760d368d8a25147b807c2196a8779ebf6338
santimat <santimaturo1@gmail.com>
2026-05-31 23:15:13 -0300

    feat: add /login <route>

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Home } from '@/pages/Home';
 import { Test } from '@/pages/Test';
 import { Header } from '@/components/layout/Header';
 import { Footer } from '@/components/layout/Footer';
+import { Login } from '@/pages/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main className="my-10 flex-1">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
           <Route path="/test" element={<Test />} />
         </Routes>
       </main>
