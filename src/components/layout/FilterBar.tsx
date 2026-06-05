07a7dd170aaa86685f7cf172cca572a0d44b56d7
santimat <santimaturo1@gmail.com>
2026-06-05 00:52:39 -0300

    feat: add chips to filter by genre

--- src/components/layout/FilterBar.tsx
+++ src/components/layout/FilterBar.tsx
@@ -1,11 +1,16 @@
 import { SearchInput } from '@/components/layout/SearchInput';
 import { SortSelect } from '@/components/layout/SortSelect';
+import { ChipFilters } from './ChipFilters';
 
 export function FilterBar() {
   return (
-    <form className="flex flex-col gap-4 p-4 font-semibold sm:max-w-80">
+    <form
+      className="flex flex-col gap-4 p-4 font-semibold sm:max-w-80"
+      onSubmit={(e) => e.preventDefault()}
+    >
       <SearchInput />
       <SortSelect />
+      <ChipFilters />
     </form>
   );
 }
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 
 export function Button({ href, className: classes, children }: ButtonProps) {
   const handleIsActive = ({ isActive }: { isActive: boolean }) => {
-    return `border-2 p-2 font-bold whitespace-nowrap transition-transform hover:-translate-x-0.5 hover:-translate-y-0.5 border-neutral active:scale-95 ${isActive && 'hidden'} ${classes}`;
+    return `border-2 p-2 font-bold whitespace-nowrap transition-transform hover:-translate-x-0.5 hover:-translate-y-0.5 border-neutral active:scale-95 ${isActive ? 'hidden' : null} ${!!classes && classes}`;
   };
   return (
     <NavLink className={handleIsActive} to={href}>
