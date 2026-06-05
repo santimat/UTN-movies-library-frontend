b44ad289f76ff2164991c0c85c5f4e208b734957
santimat <santimaturo1@gmail.com>
2026-06-04 23:06:51 -0300

    feat: move MovieList to a own component

---
+++ src/components/layout/MovieList.tsx
@@ -0,0 +1,32 @@
+import { MovieCard } from '@/components/ui/MovieCard';
+import type { MovieCardProps } from '@/types/entities/Movie';
+
+const moviesMockup: MovieCardProps[] = [
+  {
+    id: 1,
+    title: 'Spiderman No Way Home',
+    genre: 'Action',
+    year: 2021,
+    posterUrl:
+      'https://m.media-amazon.com/images/I/71gx5DiQa-L._AC_UF894,1000_QL80_.jpg',
+    averageRating: 5,
+  },
+  {
+    id: 2,
+    title: 'The Batman',
+    genre: 'Action',
+    year: 2022,
+    posterUrl: 'https://i.ebayimg.com/images/g/g3AAAOSwpIVhbMh8/s-l400.jpg',
+    averageRating: 4.5,
+  },
+];
+
+export function MovieList() {
+  return (
+    <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,0.8fr))] justify-center gap-4 p-4 md:grid-cols-[repeat(auto-fit,minmax(250px,300px))] md:justify-normal">
+      {moviesMockup.map((movie) => (
+        <MovieCard key={`movie-card-${movie.id}`} {...movie} />
+      ))}
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,21 @@
-import { MovieCard } from '@/components/ui/MovieCard';
-import type { MovieCardProps } from '@/types/entities/Movie';
-
-const moviesMockup: MovieCardProps[] = [
-  {
-    id: 1,
-    title: 'Spiderman No Way Home',
-    genre: 'Action',
-    year: 2021,
-    posterUrl:
-      'https://m.media-amazon.com/images/I/71gx5DiQa-L._AC_UF894,1000_QL80_.jpg',
-    averageRating: 5,
-  },
-  {
-    id: 2,
-    title: 'The Batman',
-    genre: 'Action',
-    year: 2022,
-    posterUrl: 'https://i.ebayimg.com/images/g/g3AAAOSwpIVhbMh8/s-l400.jpg',
-    averageRating: 4.5,
-  },
-];
+import { lazy } from 'react';
+const MovieList = lazy(() =>
+  import('@/components/layout/MovieList').then((module) => ({
+    default: module.MovieList,
+  }))
+);
+const FilterBar = lazy(() =>
+  import('@/components/layout/FilterBar').then((module) => ({
+    default: module.FilterBar,
+  }))
+);
 
 export function Home() {
   return (
     <>
       <title>Biblioteca de Películas</title>
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] justify-center gap-4 p-4 md:grid-cols-[repeat(auto-fit,minmax(250px,300px))] md:justify-normal">
-        {moviesMockup.map((movie) => (
-          <MovieCard key={`movie-card-${movie.id}`} {...movie} />
-        ))}
-      </div>
+      <FilterBar />
+      <MovieList />
     </>
   );
 }
