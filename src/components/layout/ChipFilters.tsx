47d2fe7e4decf193f7d06ecabc96f4e8bbf4dde9
santimat <santimaturo1@gmail.com>
2026-06-06 12:13:04 -0300

    feat: get genres from api

--- src/components/layout/ChipFilters.tsx
+++ src/components/layout/ChipFilters.tsx
@@ -1,35 +1,40 @@
+import { type MouseEvent, useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router';
-import type { MouseEvent } from 'react';
-
-const genresMockup = [
-  { id: 1, name: 'Action' },
-  { id: 2, name: 'Comedy' },
-  { id: 3, name: 'Drama' },
-  { id: 4, name: 'Horror' },
-  { id: 5, name: 'Sci-Fi' },
-];
+import type { Genre } from '@/types/entities/Genre';
+import { genreService } from '@/services/genres/genreService';
 
 export function ChipFilters() {
+  const [genres, setGenres] = useState<Genre[]>([]);
   const [searchParams, setSearchParams] = useSearchParams();
+  const selectedGenre = searchParams.get('genre');
+
   const handleClick = (event: MouseEvent<HTMLUListElement>) => {
     const target = event.target as HTMLElement;
     if (target.tagName !== 'BUTTON') return;
     const genre = target.dataset.genre?.toLowerCase();
     if (!genre) return;
+    if (searchParams.get('genre')?.includes(genre)) return setSearchParams('');
     setSearchParams(`?genre=${genre}`);
   };
-  const selectedGenre = searchParams.get('genre');
+
+  useEffect(() => {
+    const fetchGenres = async () => {
+      const { genres } = await genreService.getGenres();
+      setGenres(genres);
+    };
+    fetchGenres();
+  }, []);
 
   return (
     <ul
-      className="flex scrollbar-thin gap-4 overflow-x-auto pb-2"
+      className="flex scrollbar-thin gap-4 overflow-x-auto p-2"
       onClick={handleClick}
     >
-      {genresMockup.map((genre) => (
+      {genres.map((genre) => (
         <li key={`genre-chip-${genre.id}`}>
           <button
             data-genre={genre.name}
-            className={`hover:cursor border-2 border-neutral px-2 whitespace-nowrap transition-transform hover:-translate-x-0.5 hover:-translate-y-0.5 hover:cursor-pointer active:scale-95 ${selectedGenre?.includes(genre.name.toLowerCase()) ? 'bg-neutral text-white' : ''}`}
+            className={`hover:cursor border-2 border-neutral px-2 whitespace-nowrap transition-transform first-letter:uppercase hover:-translate-x-0.5 hover:-translate-y-0.5 hover:cursor-pointer active:scale-95 ${selectedGenre?.includes(genre.name.toLowerCase()) ? 'bg-neutral text-white' : ''}`}
           >
             {genre.name}
           </button>
