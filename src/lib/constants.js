5285d0f519f8228750c056d9ca589fbc8fbe2c18
santimat <santimaturo1@gmail.com>
2026-06-06 19:18:42 -0300

    feat: create a validator service and a constants file

---
+++ src/lib/constants.js
@@ -0,0 +1,12 @@
+export const REGISTER_FIELDS = {
+  username: 'Nombre de usuario',
+  email: 'Correo electrónico',
+  password: 'Contraseña',
+};
+
+export const LOGIN_FIELDS = {
+  email: 'Correo electrónico',
+  password: 'Contraseña',
+};
+
+export const API_URL = import.meta.env.VITE_API_URL;
---
+++ src/services/validateService.ts
@@ -0,0 +1,22 @@
+import { toast } from 'sonner';
+
+export const validateService = {
+  areMissingFields: (
+    formData: FormData,
+    FIELDS_DICTIONARY: Record<string, string>
+  ) => {
+    const missingFields = Array.from(formData.keys()).filter((field) => {
+      const value = formData.get(field);
+      return !value || value.toString().trim() === '';
+    });
+
+    if (missingFields.length) {
+      const missingFieldNames = missingFields.map((field) => {
+        return FIELDS_DICTIONARY[field as keyof typeof FIELDS_DICTIONARY];
+      });
+      return toast.error(
+        `Por favor, completa los siguientes campos: ${missingFieldNames.join(', ')}`
+      );
+    }
+  },
+};
