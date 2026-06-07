5e3e05da7e9e06a372e971169ba3814485d42161
santimat <santimaturo1@gmail.com>
2026-06-07 18:05:44 -0300

    refactor: delete css folder

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import { Toaster } from 'sonner';
 import App from '@/App.tsx';
-import '@/css/global.css';
+import '@/global.css';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
