614f1742aecb8f2ef204eb094830f4e0cd09d496
santimat <santimaturo1@gmail.com>
2026-06-07 19:19:36 -0300

    feat: add camera off icon

---
+++ src/shared/components/icons/CameraOff.tsx
@@ -0,0 +1,25 @@
+import type { SvgProps } from '@/shared/types';
+
+export function CameraOffIcon({
+  width = 32,
+  height = 32,
+  className: classes,
+}: SvgProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      className={classes}
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="square"
+        stroke-width="2"
+        d="M16 10.2v.801l.001.001zm0 0V5.001l-4.999.001m0 0h-1L10 5.001zm12 10.998V6L16.5 9.9M23 16v1l.001.001zm-7 0v3H1V5h4zm5 5L3 3"
+      />
+    </svg>
+  );
+}
