318802f10be19fd675b6160f58ada44d6d08917b
santimat <santimaturo1@gmail.com>
2026-06-07 19:20:31 -0300

    fix: make loader absolute to prevent collapse

--- src/shared/components/ui/Loader.tsx
+++ src/shared/components/ui/Loader.tsx
@@ -2,7 +2,7 @@ import { LoaderIcon } from '@/shared/components/icons/Loader';
 
 export function Loader() {
   return (
-    <div className="flex h-full animate-spin items-center justify-center">
+    <div className="absolute inset-0 flex animate-spin items-center justify-center">
       <LoaderIcon className="stroke-neutral" />
     </div>
   );
