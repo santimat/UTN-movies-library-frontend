fcb4fcdac4c23014b287d712abc4b0b88dbcecbe
santimat <santimaturo1@gmail.com>
2026-06-07 15:29:27 -0300

    feat: add loader component

---
+++ src/components/ui/Loader.tsx
@@ -0,0 +1,9 @@
+import { LoaderIcon } from '@/components/icons/Loader';
+
+export function Loader() {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <LoaderIcon />
+    </div>
+  );
+}
