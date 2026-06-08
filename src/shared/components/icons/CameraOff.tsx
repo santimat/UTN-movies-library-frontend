bb65d9608de3712ec989f917ac0df89fced65e8c
santimat <santimaturo1@gmail.com>
2026-06-07 21:29:12 -0300

    fix: svg attributes

--- src/shared/components/icons/CameraOff.tsx
+++ src/shared/components/icons/CameraOff.tsx
@@ -16,8 +16,8 @@ export function CameraOffIcon({
       <path
         fill="none"
         stroke="currentColor"
-        stroke-linecap="square"
-        stroke-width="2"
+        strokeLinecap="square"
+        strokeWidth="2"
         d="M16 10.2v.801l.001.001zm0 0V5.001l-4.999.001m0 0h-1L10 5.001zm12 10.998V6L16.5 9.9M23 16v1l.001.001zm-7 0v3H1V5h4zm5 5L3 3"
       />
     </svg>
