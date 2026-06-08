22ac6794330d1cc60a17968a7305bac9456a9a20
santimat <santimaturo1@gmail.com>
2026-06-08 10:33:46 -0300

    feat: add error hanlding

--- src/features/movies/services/genreService.ts
+++ src/features/movies/services/genreService.ts
@@ -1,11 +1,16 @@
 import { API_URL } from '@/shared/utils/constants';
 import type { SpringPageResponse } from '@/shared/types';
+import { handleFetchErrors } from '@/shared/utils/handleFetchErrors';
 
 const URL_BASE = `${API_URL}/genres`;
 export const genreService = {
   getGenres: async () => {
-    const res = await fetch(URL_BASE);
-    const data: SpringPageResponse = await res.json();
-    return { genres: data.content };
+    try {
+      const res = await fetch(URL_BASE);
+      const data: SpringPageResponse = await res.json();
+      return { genres: data.content };
+    } catch (error) {
+      handleFetchErrors(error);
+    }
   },
 };
