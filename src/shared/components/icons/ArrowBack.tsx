515b4b86e36853103593ff86e624dab6e36c33b9
santimat <santimaturo1@gmail.com>
2026-06-07 21:30:33 -0300

    feat: add arrow back icon

---
+++ src/shared/components/icons/ArrowBack.tsx
@@ -0,0 +1,26 @@
+import type { SvgProps } from '@/shared/types';
+
+export function ArrowBackIcon({
+  width = 32,
+  height = 32,
+  className: classes,
+}: SvgProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      className={classes}
+      viewBox="0 0 512 512"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="48"
+        d="M244 400L100 256l144-144M120 256h292"
+      />
+    </svg>
+  );
+}
