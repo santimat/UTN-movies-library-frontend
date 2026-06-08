ab98ee5a893ab70ae449772a36f7f7ce8c094b1e
santimat <santimaturo1@gmail.com>
2026-06-08 10:57:19 -0300

    refactor: delete genre service

--- src/features/movies/components/GenreFilters.tsx
+++ src/features/movies/components/GenreFilters.tsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router';
-import { genreService } from '@/features/movies/services/genreService';
-import { type Genre } from '@/features/movies/types';
 import { Button } from '@/shared/components/ui/Button';
+import { useMoviesStore } from '@/features/movies/store/useMoviesStore';
 export function ChipFilters() {
-  const [genres, setGenres] = useState<Genre[]>([]);
   const [searchParams, setSearchParams] = useSearchParams();
+  const { movies } = useMoviesStore();
   const selectedGenre = searchParams.get('genre');
+
+  const genres: string[] = [
+    ...new Set(movies.map((movie) => movie.genre.toLowerCase())),
+  ];
 
   const handleClick = (genre: string) => {
     if (selectedGenre == genre) {
@@ -21,27 +23,17 @@ export function ChipFilters() {
     });
   };
 
-  useEffect(() => {
-    const fetchGenres = async () => {
-      const { genres } = await genreService.getGenres();
-      setGenres(genres);
-    };
-    fetchGenres();
-  }, []);
-
   return (
     <ul className="flex scrollbar-thin gap-4 overflow-x-auto p-2">
       {genres.map((genre) => (
-        <li key={`chíp-genre-${genre.id}`}>
+        <li key={`chíp-genre-${genre}`}>
           <Button
-            onClick={() => handleClick(genre.name.toLowerCase())}
+            onClick={() => handleClick(genre.toLowerCase())}
             className={`hover:cursor px-2 py-0 first-letter:uppercase hover:-translate-x-0.5 hover:-translate-y-0.5 hover:cursor-pointer active:scale-95 ${
-              selectedGenre?.includes(genre.name.toLowerCase())
-                ? 'bg-neutral text-white'
-                : ''
+              selectedGenre?.includes(genre) ? 'bg-neutral text-white' : ''
             } `}
           >
-            {genre.name}
+            {genre}
           </Button>
         </li>
       ))}
