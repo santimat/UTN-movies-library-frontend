2e3b82cde7cca3df8bc3efd40e4ad49d8da06ae2
santimat <santimaturo1@gmail.com>
2026-06-07 23:25:28 -0300

    fix: use useLocation instead location API

--- src/routes/GuardRoute.tsx
+++ src/routes/GuardRoute.tsx
@@ -2,10 +2,11 @@ import { Outlet, Navigate } from 'react-router';
 import { useEffect } from 'react';
 import { useAuthStore } from '@/features/auth/store/useAuthStore';
 import { Loader } from '@/shared/components/ui/Loader';
+import { useLocation } from 'react-router';
 export function GuardRoute() {
   const { user, loading, hydrateUser } = useAuthStore();
 
-  const isLoginPage = window.location.pathname === '/auth';
+  const isLoginPage = useLocation().pathname.split('/')[1] === 'auth';
   useEffect(() => {
     hydrateUser();
   }, [hydrateUser]);
