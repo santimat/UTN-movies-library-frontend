c362739fc1a0e385cebb5ad5119c411d5b1664d1
santimat <santimaturo1@gmail.com>
2026-06-08 10:34:11 -0300

    feat: update movies fetching

--- src/features/movies/components/MovieList.tsx
+++ src/features/movies/components/MovieList.tsx
@@ -6,21 +6,40 @@ import { useMoviesStore } from '@/features/movies/store/useMoviesStore';
 import { type Movie } from '@/features/movies/types';
 
 export function MovieList() {
-  const { movies, fetchMovies, loading } = useMoviesStore();
   const [searchParams] = useSearchParams();
+  const { movies, fetchMovies, loading, error } = useMoviesStore();
+
+  const genre = searchParams.get('genre') || undefined;
+  const sortBy = (searchParams.get('sortBy') as keyof Movie) || undefined;
+  const sortOrder =
+    (searchParams.get('sortOrder') as 'ASC' | 'DESC') || undefined;
+  const searchText = searchParams.get('searchText') || undefined;
 
   useEffect(() => {
-    const genre = searchParams.get('genre') || undefined;
-    const sortBy = (searchParams.get('sortBy') as keyof Movie) || undefined;
-    const sortOrder =
-      (searchParams.get('sortOrder') as 'ASC' | 'DESC') || undefined;
-    const searchText = searchParams.get('searchText') || undefined;
     fetchMovies({ genre, sortBy, sortOrder, searchText });
-  }, [fetchMovies, searchParams]);
+  }, [searchText, genre, sortBy, sortOrder, fetchMovies]);
 
   if (loading) {
     return <Loader />;
   }
+
+  if (error)
+    return (
+      <div className="flex h-full items-center justify-center">
+        <p className="font-headline text-2xl font-bold text-secondary uppercase">
+          {error.error} 🥲
+        </p>
+      </div>
+    );
+
+  if (!movies.length)
+    return (
+      <div className="flex h-full items-center justify-center">
+        <p className="font-headline text-2xl font-bold text-neutral uppercase">
+          No se encontraron peliculas.
+        </p>
+      </div>
+    );
 
   return (
     <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,0.8fr))] justify-center gap-4 p-4 md:justify-normal xl:grid-cols-[repeat(auto-fit,minmax(250px,300px))]">
