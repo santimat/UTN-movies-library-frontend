8c61928f791ef86deb5158f059a0827638b11080
santimat <santimaturo1@gmail.com>
2026-06-08 01:39:44 -0300

    feat: create button component

--- src/shared/components/ui/Button.tsx
+++ src/shared/components/ui/Button.tsx
@@ -1,19 +1,15 @@
-import type { ReactNode } from 'react';
-import { NavLink } from 'react-router';
-
-interface ButtonProps {
-  children: ReactNode;
-  href: string;
+type ButtonProps = {
+  children: React.ReactNode;
   className?: string;
-}
-
-export function Button({ href, className: classes, children }: ButtonProps) {
-  const handleIsActive = ({ isActive }: { isActive: boolean }) => {
-    return `border-2 p-2 font-bold whitespace-nowrap transition-transform hover:-translate-x-0.5 hover:-translate-y-0.5 border-neutral active:scale-95 ${isActive ? 'hidden' : null} ${!!classes && classes}`;
-  };
+  onClick?: () => void;
+};
+export function Button({ children, className: classes, onClick }: ButtonProps) {
   return (
-    <NavLink className={handleIsActive} to={href}>
+    <button
+      onClick={onClick}
+      className={`${classes} cursor-pointer border-2 border-neutral p-2 shadow-auth transition-transform hover:-translate-x-0.5 hover:-translate-y-0.5 active:scale-95`}
+    >
       {children}
-    </NavLink>
+    </button>
   );
 }
