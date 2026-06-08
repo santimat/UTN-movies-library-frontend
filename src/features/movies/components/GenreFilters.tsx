2eeefbc94b966490fddcf7b58ba8636634badae3
santimat <santimaturo1@gmail.com>
2026-06-08 10:58:42 -0300

    fix: use store to get genres and errors

--- src/features/movies/components/GenreFilters.tsx
+++ src/features/movies/components/GenreFilters.tsx
@@ -1,14 +1,11 @@
+import { useEffect } from 'react';
 import { useSearchParams } from 'react-router';
 import { Button } from '@/shared/components/ui/Button';
 import { useMoviesStore } from '@/features/movies/store/useMoviesStore';
 export function ChipFilters() {
   const [searchParams, setSearchParams] = useSearchParams();
-  const { movies } = useMoviesStore();
+  const { genres, fetchGenres, error } = useMoviesStore();
   const selectedGenre = searchParams.get('genre');
-
-  const genres: string[] = [
-    ...new Set(movies.map((movie) => movie.genre.toLowerCase())),
-  ];
 
   const handleClick = (genre: string) => {
     if (selectedGenre == genre) {
@@ -23,20 +20,30 @@ export function ChipFilters() {
     });
   };
 
+  useEffect(() => {
+    fetchGenres();
+  }, [fetchGenres]);
+
   return (
     <ul className="flex scrollbar-thin gap-4 overflow-x-auto p-2">
-      {genres.map((genre) => (
-        <li key={`chíp-genre-${genre}`}>
-          <Button
-            onClick={() => handleClick(genre.toLowerCase())}
-            className={`hover:cursor px-2 py-0 first-letter:uppercase hover:-translate-x-0.5 hover:-translate-y-0.5 hover:cursor-pointer active:scale-95 ${
-              selectedGenre?.includes(genre) ? 'bg-neutral text-white' : ''
-            } `}
-          >
-            {genre}
-          </Button>
-        </li>
-      ))}
+      {error?.code ? (
+        <li className="text-red-500">Error loading genres: {error.error}</li>
+      ) : (
+        genres.map((genre) => (
+          <li key={`chíp-genre-${genre.id}`}>
+            <Button
+              onClick={() => handleClick(genre?.name.toLowerCase())}
+              className={`hover:cursor px-2 py-0 first-letter:uppercase hover:-translate-x-0.5 hover:-translate-y-0.5 hover:cursor-pointer active:scale-95 ${
+                selectedGenre === genre?.name.toLowerCase()
+                  ? 'bg-neutral text-white'
+                  : ''
+              } `}
+            >
+              {genre?.name}
+            </Button>
+          </li>
+        ))
+      )}
     </ul>
   );
 }
