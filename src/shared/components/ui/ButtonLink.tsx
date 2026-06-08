5c82f16243bcc31bc1cf06ab9e7db0ab00d497dc
santimat <santimaturo1@gmail.com>
2026-06-08 01:39:37 -0300

    refactor: rename button to buttonLink

---
+++ src/shared/components/ui/ButtonLink.tsx
@@ -0,0 +1,23 @@
+import type { ReactNode } from 'react';
+import { NavLink } from 'react-router';
+
+interface ButtonProps {
+  children: ReactNode;
+  href: string;
+  className?: string;
+}
+
+export function ButtonLink({
+  href,
+  className: classes,
+  children,
+}: ButtonProps) {
+  const handleIsActive = ({ isActive }: { isActive: boolean }) => {
+    return `border-2 p-2 font-bold whitespace-nowrap transition-transform hover:-translate-x-0.5 hover:-translate-y-0.5 border-neutral active:scale-95 ${isActive ? 'hidden' : ''} ${!!classes && classes}`;
+  };
+  return (
+    <NavLink className={handleIsActive} to={href}>
+      {children}
+    </NavLink>
+  );
+}
