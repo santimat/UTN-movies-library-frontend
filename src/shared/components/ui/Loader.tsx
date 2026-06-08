d8e800cd216ee80623175a6246d987376bb08d77
santimat <santimaturo1@gmail.com>
2026-06-07 21:29:23 -0300

    fix: overflow loader

--- src/shared/components/ui/Loader.tsx
+++ src/shared/components/ui/Loader.tsx
@@ -2,8 +2,10 @@ import { LoaderIcon } from '@/shared/components/icons/Loader';
 
 export function Loader() {
   return (
-    <div className="absolute inset-0 flex animate-spin items-center justify-center">
-      <LoaderIcon className="stroke-neutral" />
+    <div className="absolute inset-0 flex items-center justify-center overflow-hidden">
+      <span className="animate-spin">
+        <LoaderIcon className="stroke-neutral" />
+      </span>
     </div>
   );
 }
