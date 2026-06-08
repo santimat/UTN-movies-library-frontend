6b720eca3ff7a409ba5d39b111c89b7e720d4ed2
santimat <santimaturo1@gmail.com>
2026-06-08 01:38:21 -0300

    fix: add to shared types spring page response and add trailer url to movie typo

--- src/features/movies/types.ts
+++ src/features/movies/types.ts
@@ -5,37 +5,17 @@ export type Movie = {
   genre: string;
   synopsis: string;
   year: number;
-  posterUrl: string;
   averageRating: number;
   duration: number;
+  posterUrl: string;
+  trailerUrl: string;
 };
 
 export type MovieRequest = Omit<Movie, 'id'>;
 export type MovieResponse = Movie;
 export type MovieCardProps = Omit<Movie, 'director' | 'synopsis'>;
 
-export type SpringPageResponse = {
-  content: [];
-  page: {
-    size: number;
-    number: number;
-    totalElements: number;
-    totalPages: number;
-  };
-};
-
 export type Genre = {
   id: number;
   name: string;
 };
-
-export type Review = {
-  id: number;
-  rating: number;
-  comment: string;
-  movieId: number;
-  movieTitle: string;
-  username: string;
-};
-export type ReviewRequest = Omit<Review, 'id' | 'username' | 'movieTitle'>;
-export type ReviewResponse = Review;
