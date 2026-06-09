bc79f00a58e0530e3476fdd11c6d5eb1fd2aa641
santimat <santimaturo1@gmail.com>
2026-06-09 16:48:18 -0300

    refactor: translate duration to duración

--- src/features/movies/components/MovieDetail.tsx
+++ src/features/movies/components/MovieDetail.tsx
@@ -43,7 +43,7 @@ export function MovieDetail({ movie }: MovieDetailProps) {
             <MovieMetaItem label="director" value={movie?.director} />
           </li>
           <li className="w-full flex-1">
-            <MovieMetaItem label="duration" value={`${movie?.duration} mins`} />
+            <MovieMetaItem label="duración" value={`${movie?.duration} mins`} />
           </li>
         </ul>
         <div className="mt-6 flex flex-col gap-4 md:flex-row">
