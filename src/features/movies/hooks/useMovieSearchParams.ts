f117ecc9dcfe3946aecaa5348e52dd25e563fa27
santimat <santimaturo1@gmail.com>
2026-06-10 19:46:12 -0300

    refactor: improve set search params handling creating a helper function, and adding useCallback to prevent re-renders

--- src/features/movies/hooks/useMovieSearchParams.ts
+++ src/features/movies/hooks/useMovieSearchParams.ts
@@ -1,4 +1,10 @@
-import { type ChangeEvent, useState, useRef, useEffect } from 'react';
+import {
+  type ChangeEvent,
+  useState,
+  useRef,
+  useEffect,
+  useCallback,
+} from 'react';
 import { useSearchParams } from 'react-router';
 import { type Movie } from '@/features/movies/types';
 import { DEBOUNCE_TIME } from '@/shared/utils/constants';
@@ -6,45 +12,49 @@ import { DEBOUNCE_TIME } from '@/shared/utils/constants';
 export function useMovieSearchParams() {
   const timeoutRef = useRef<number>(0);
   const [searchParams, setSearchParams] = useSearchParams();
-  const [inputValue, setInputValue] = useState<string>(
-    searchParams.get('searchText') || ''
-  );
+
   const genre = searchParams.get('genre') || undefined;
   const sortBy = (searchParams.get('sortBy') as keyof Movie) || 'title';
   const sortOrder = (searchParams.get('sortOrder') as 'ASC' | 'DESC') || 'ASC';
   const page = searchParams.get('page') || '0';
-  const searchText = searchParams.get('searchText') || undefined;
+  const searchText = searchParams.get('searchText') || '';
+
+  const [inputValue, setInputValue] = useState<string>(searchText);
+
+  const updateSearchParam = useCallback(
+    (filter: Record<string, string>) => {
+      setSearchParams((prev) => {
+        const { key, value } = filter;
+        const nextParams = new URLSearchParams(prev);
+        if (!value) nextParams.delete(key);
+        else nextParams.set(key, value);
+
+        return nextParams;
+      });
+    },
+    [setSearchParams]
+  );
 
   const setSortBy = (value: string) => {
-    setSearchParams((prev) => {
-      const next = new URLSearchParams(prev);
-      next.set('sortBy', value);
-      return next;
-    });
+    updateSearchParam({ key: 'sortBy', value });
   };
 
-  const setSortOrder = () => {
-    setSearchParams((prev) => {
-      const next = new URLSearchParams(prev);
-      const nextSortOrder = sortOrder === 'ASC' ? 'DESC' : 'ASC';
-      next.set('sortOrder', nextSortOrder);
-      return next;
-    });
-  };
+  const setSortOrder = useCallback(() => {
+    const value = sortOrder === 'ASC' ? 'DESC' : 'ASC';
+    updateSearchParam({ key: 'sortOrder', value });
+  }, [sortOrder, updateSearchParam]);
 
-  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value.toLowerCase());
-  };
+  const handleSearchChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      setInputValue(event.target.value.toLowerCase());
+    },
+    []
+  );
 
   useEffect(() => {
     if (timeoutRef.current) clearTimeout(timeoutRef.current);
     timeoutRef.current = setTimeout(() => {
-      setSearchParams((prevParams) => {
-        const next = new URLSearchParams(prevParams);
-        if (inputValue == '') next.delete('searchText');
-        else next.set('searchText', inputValue);
-        return next;
-      });
+      updateSearchParam({ key: 'searchText', value: inputValue });
     }, DEBOUNCE_TIME);
 
     return () => clearTimeout(timeoutRef.current);
@@ -61,5 +71,6 @@ export function useMovieSearchParams() {
     setSortBy,
     setSortOrder,
     handleSearchChange,
+    updateSearchParam,
   };
 }
