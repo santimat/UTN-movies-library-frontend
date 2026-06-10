ec8ab44fc73759f2b698a344112b654c97db644c
santimat <santimaturo1@gmail.com>
2026-06-10 19:45:26 -0300

    refactor: improve set search params handling

--- src/features/movies/components/GenreFilters.tsx
+++ src/features/movies/components/GenreFilters.tsx
@@ -3,9 +3,10 @@ import { useSearchParams } from 'react-router';
 import { Button } from '@/shared/components/ui/Button';
 import { useMoviesStore } from '@/features/movies/store/useMoviesStore';
 import { useShallow } from 'zustand/shallow';
+import { useMovieSearchParams } from '@/features/movies/hooks/useMovieSearchParams';
 
 export function GenreFilters() {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const { updateSearchParam, genre: genreFromParam } = useMovieSearchParams();
   const { genres, fetchGenres, error } = useMoviesStore(
     useShallow((state) => ({
       genres: state.genres,
@@ -13,21 +14,12 @@ export function GenreFilters() {
       error: state.genresError,
     }))
   );
-  const selectedGenre = searchParams.get('genre');
 
   const handleClick = (genre: string) => {
-    if (selectedGenre === genre) {
-      return setSearchParams((prev) => {
-        const next = new URLSearchParams(prev);
-        next.delete('genre');
-        return next;
-      });
+    if (genreFromParam === genre) {
+      return updateSearchParam({ key: 'genre', value: '' });
     }
-    setSearchParams((prev) => {
-      const next = new URLSearchParams(prev);
-      next.set('genre', genre);
-      return next;
-    });
+    updateSearchParam({ key: 'genre', value: genre });
   };
 
   useEffect(() => {
@@ -44,7 +36,7 @@ export function GenreFilters() {
             <Button
               onClick={() => handleClick(genre?.name.toLowerCase())}
               className={`px-2 py-0 first-letter:uppercase hover:-translate-x-0.5 hover:-translate-y-0.5 hover:cursor-pointer active:scale-95 ${
-                selectedGenre === genre?.name.toLowerCase()
+                genreFromParam === genre?.name.toLowerCase()
                   ? 'bg-neutral text-white'
                   : ''
               } `}
