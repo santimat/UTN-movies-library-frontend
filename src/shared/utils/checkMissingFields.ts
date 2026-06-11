df5c66d785072ab906bea5091d39ba6b6b9593a6
santimat <santimaturo1@gmail.com>
2026-06-11 10:39:36 -0300

    refactor: change to Object value check

--- src/shared/utils/checkMissingFields.ts
+++ src/shared/utils/checkMissingFields.ts
@@ -1,20 +1,16 @@
-import { toast } from 'sonner';
-
 export const areMissingFields = (
-  formData: FormData,
+  payload: Record<string, unknown>,
   FIELDS_DICTIONARY: Record<string, string>
 ) => {
-  const missingFields = Array.from(formData.keys()).filter((field) => {
-    const value = formData.get(field);
+  const missingFields = Object.keys(payload).filter((field) => {
+    const value = payload[field];
+    if (typeof value === 'boolean') return false;
     return !value || value.toString().trim() === '';
   });
 
   if (missingFields.length) {
-    const missingFieldNames = missingFields.map((field) => {
+    return missingFields.map((field) => {
       return FIELDS_DICTIONARY[field as keyof typeof FIELDS_DICTIONARY];
     });
-    return toast.error(
-      `Por favor, completa los siguientes campos: ${missingFieldNames.join(', ')}`
-    );
   }
 };
