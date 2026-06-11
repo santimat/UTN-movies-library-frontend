2dec3ab89c0a6766e30cafd5993717ca29d28f69
santimat <santimaturo1@gmail.com>
2026-06-11 13:05:20 -0300

    feat: init with pagination

--- src/features/movies/components/Pagination.tsx
+++ src/features/movies/components/Pagination.tsx
@@ -1,12 +1,19 @@
 import { useMoviesStore } from '@/features/movies/store/useMoviesStore';
+import { ButtonLink } from '@/shared/components/ui/ButtonLink';
 
 export function Pagination() {
   const totalPages = useMoviesStore((s) => s.data.totalPages);
-  if (!totalPages || totalPages < 1) return null;
-
+  if (!totalPages || totalPages <= 1) return null;
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
   return (
-    <ul>
-      <li></li>
+    <ul className="mt-6 flex justify-center gap-2">
+      {pages.map((page) => (
+        <li key={`link-page-${page}`}>
+          <ButtonLink to={`?page=${page}`} className="py-1">
+            {page}
+          </ButtonLink>
+        </li>
+      ))}
     </ul>
   );
 }
