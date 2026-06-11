247ccdee6776fba37223a01b95908f90d72d37aa
santimat <santimaturo1@gmail.com>
2026-06-11 10:36:02 -0300

    refactor: use .ts for constants

---
+++ src/shared/utils/constants.ts
@@ -0,0 +1,18 @@
+export const REGISTER_FIELDS = {
+  name: 'Nombre de usuario',
+  email: 'Correo electrónico',
+  password: 'Contraseña',
+};
+
+export const LOGIN_FIELDS = {
+  email: 'Correo electrónico',
+  password: 'Contraseña',
+};
+export const REVIEW_FIELDS = {
+  rating: 'Calificación',
+  comment: 'Comentario',
+};
+
+export const API_URL = import.meta.env.VITE_API_URL;
+
+export const DEBOUNCE_TIME = 400;
