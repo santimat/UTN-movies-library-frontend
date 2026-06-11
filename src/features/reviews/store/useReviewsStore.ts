dcc7e45af3c06847089665ebd5d30a46cdfb26bb
santimat <santimaturo1@gmail.com>
2026-06-11 11:19:13 -0300

    feat: add create movie and isUserReview method in review store

--- src/features/reviews/store/useReviewsStore.ts
+++ src/features/reviews/store/useReviewsStore.ts
@@ -1,20 +1,40 @@
 import { create } from 'zustand';
-import { type Review } from '@/features/reviews/types';
+import { type Review, type ReviewRequest } from '@/features/reviews/types';
 import { reviewService } from '@/features/reviews/services/reviewService';
+import type { AppError } from '@/shared/types';
 
 type ReviewsState = {
   reviews: Review[];
+  isLoading: boolean;
+  errorReview: AppError | null;
   fetchReviews: (movieId: number) => Promise<void>;
+  createReview: (review: ReviewRequest) => Promise<void>;
+  isUserReview: (userId: number) => boolean;
 };
 
-export const useReviewsStore = create<ReviewsState>((set) => ({
+export const useReviewsStore = create<ReviewsState>((set, get) => ({
   reviews: [],
+  isLoading: true,
+  errorReview: null,
   fetchReviews: async (movieId) => {
     try {
       const reviews = await reviewService.getReviewsByMovieId(movieId);
-      set({ reviews });
+      set({ reviews, isLoading: false });
     } catch {
-      set({ reviews: [] });
+      set({ reviews: [], isLoading: false });
     }
+  },
+  createReview: async (review: ReviewRequest) => {
+    try {
+      const newReview = await reviewService.createReview(review);
+      set((state) => ({ reviews: [...state.reviews, newReview] }));
+    } catch (error) {
+      set({ errorReview: error as AppError, isLoading: false });
+    }
+  },
+  isUserReview: (userId: number) => {
+    if (!userId) return false;
+    const { reviews } = get();
+    return reviews.some((review) => review.userId === userId);
   },
 }));
