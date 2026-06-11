a137b62e1db62912f55b78539720dfda73f971c9
santimat <santimaturo1@gmail.com>
2026-06-11 00:51:03 -0300

    feat: create Rating Input with stars

---
+++ src/features/reviews/components/RatingInput.tsx
@@ -0,0 +1,33 @@
+import { StarIcon } from '@/shared/components/icons/Star';
+
+const STARS = [1, 2, 3, 4, 5];
+
+type RatingInputProps = {
+  rating: number;
+  onClick: (rating: number) => void;
+};
+export function RatingInput({
+  rating,
+  onClick: handleClick,
+}: RatingInputProps) {
+  return (
+    <div>
+      <label className="text-xl">Rating</label>
+      <ul className="flex justify-around border-2 border-neutral p-2">
+        {STARS.map((star) => (
+          <li
+            className="active:scale-95"
+            key={`star-input-${star}`}
+            onClick={() => handleClick(star)}
+          >
+            <StarIcon
+              width={45}
+              height={45}
+              className={`pointer-events-none ${star <= rating ? 'fill-amber-400' : ''} `}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
