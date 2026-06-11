64a7cc33dd01bb7446ae0b5087e1dbdc8d486340
santimat <santimaturo1@gmail.com>
2026-06-11 00:50:25 -0300

    refactor: change width on medium media query

--- src/features/reviews/components/MovieFeedback.tsx
+++ src/features/reviews/components/MovieFeedback.tsx
@@ -2,7 +2,7 @@ import { ReviewList } from '@/features/reviews/components/ReviewList';
 import { ReviewForm } from '@/features/reviews/components/ReviewForm';
 export function MovieFeedback({ movieId }: { movieId: number }) {
   return (
-    <section className="mx-auto grid w-[95%] px-4 font-headline font-bold md:grid-cols-2">
+    <section className="mx-auto grid w-[95%] gap-10 px-4 font-headline font-bold text-neutral md:w-[80%] lg:grid-cols-2">
       <ReviewList movieId={movieId} />
       <ReviewForm />
     </section>
