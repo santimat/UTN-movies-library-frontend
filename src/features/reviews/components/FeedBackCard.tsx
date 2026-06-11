daf0163bb8a98f19c8eda189772101dc473a0d1b
santimat <santimaturo1@gmail.com>
2026-06-11 10:37:53 -0300

    feat: check if logged's user id match with user id in review

--- src/features/reviews/components/FeedBackCard.tsx
+++ src/features/reviews/components/FeedBackCard.tsx
@@ -1,10 +1,13 @@
 import { type Review } from '@/features/reviews/types';
 import { Stars } from '@/features/reviews/components/Stars';
+import { useAuthStore } from '@/features/auth/store/useAuthStore';
 
 const COLORS = ['bg-secondary', 'bg-tertiary', 'bg-neutral', 'bg-gray-900'];
 
 export function FeedBackCard({ review }: { review: Review }) {
   const randomColor = COLORS[review.id % COLORS.length];
+  const userId = useAuthStore((s) => s.user?.id);
+  const isUserReview = userId === review.userId;
 
   return (
     <article className="w-full border-2 border-neutral p-2 shadow-auth">
@@ -20,6 +23,9 @@ export function FeedBackCard({ review }: { review: Review }) {
           </h3>
         </div>
         <Stars rating={review.rating} />
+        {isUserReview && (
+          <small className="text-sm text-neutral/60">Tu reseña</small>
+        )}
       </div>
       <p className="mt-4 text-neutral/60 first-letter:uppercase">
         {review.comment}
