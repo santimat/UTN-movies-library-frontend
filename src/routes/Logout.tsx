c415c6d9cdf5ad825dbdc72bb780392b7ebf109b
santimat <santimaturo1@gmail.com>
2026-06-23 09:59:26 -0300

    refactor: improve errro handling

--- src/routes/Logout.tsx
+++ src/routes/Logout.tsx
@@ -1,12 +1,22 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router';
 import { useAuthStore } from '@/features/auth/store/useAuthStore';
+import { sileo } from 'sileo';
+import type { AppError } from '@/shared/types';
 export function Logout() {
   const logout = useAuthStore((s) => s.logout);
   const navigate = useNavigate();
   useEffect(() => {
     const performLogout = async () => {
-      await logout();
+      try {
+        await logout();
+      } catch (err) {
+        const { error } = err as AppError;
+        sileo.error({
+          title: 'Error al cerrar sesión',
+          description: error,
+        });
+      }
     };
     performLogout();
     navigate('/');
