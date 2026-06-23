8fa40fea4f6ff3aac63bee8f235d54d2c81bc14c
santimat <santimaturo1@gmail.com>
2026-06-22 22:46:26 -0300

    refactor: add mb-10 to forms container

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -7,7 +7,7 @@ export function Auth() {
       <h1 className="mb-10 text-center font-body text-2xl font-bold uppercase md:text-3xl">
         Accede a tu <span className="text-secondary">biblioteca</span>
       </h1>
-      <div className="flex flex-col justify-center gap-8 font-body md:flex-row md:items-start md:px-10">
+      <div className="mb-10 flex flex-col justify-center gap-8 font-body md:flex-row md:items-start md:px-10">
         <LoginForm />
         <RegisterForm />
       </div>
