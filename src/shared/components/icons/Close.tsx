5308ea0981ef8ca88fe72c37dddafa1cf8516be1
santimat <santimaturo1@gmail.com>
2026-06-25 19:24:28 -0300

    fix: attributes

--- src/shared/components/icons/Close.tsx
+++ src/shared/components/icons/Close.tsx
@@ -16,8 +16,8 @@ export function CloseIcon({
       <path
         fill="currentColor"
         stroke="currentColor"
-        stroke-linecap="square"
-        stroke-width="2"
+        strokeLinecap="square"
+        strokeWidth="2"
         d="M16.95 7.05L12 12m0 0l-4.95 4.95M12 12l4.95 4.95M12 12L7.05 7.05"
       />
     </svg>
